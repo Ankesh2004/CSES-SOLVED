ccd37614a14c9fff7cbb0388b7e7837aab685767
Ankesh2004 <gankesh808@gmail.com>
2025-05-27 13:12:42 +0530

    solved

---
+++ SORTING /Nearest_Smaller_Values.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define lld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define fri(n) for (int i = 0; i < n; i++)
+#define sortv(v) sort((v).begin(), (v).end())
+#define printv(v, start_idx)                   \
+    for (int i = start_idx; i < v.size(); i++) \
+        cout << v[i] << " ";
+ 
+ll mod = 1e9 + 7;
+const ll MAXN = 1002;
+ 
+ 
+void solve() {
+    ll n;
+    cin>>n;
+    vector<
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    solve();
+    return 0;
+}
